23a24f6254ef6c02b32e11f061780aef9ef710ed
glptrst <giulio.pietroiusti@gmail.com>
2022-01-06 23:48:21 +0100

    Basic example.

---
+++ src/public/javascripts/like_button.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    // Using JSX
+    return (
+	<button onClick={() => this.setState({ liked: true })}>
+	Like
+      </button>
+    );
+
+    // Without JSX
+    // return e(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   'Like'
+    // );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
